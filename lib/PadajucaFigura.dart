85550bec0bb3434bb581a1ec6ce82f1b0c89e6f8
Lazar <you@example.com>
2023-09-24 08:19:57 +0200

    za teodora

--- lib/PadajucaFigura.dart
+++ lib/PadajucaFigura.dart
@@ -12,27 +12,34 @@ class PadajucaFigura extends Figura {
   // static final String pathDoSlikePadajuceFigure = 'assets/apple.png';
   static final List<String> pathovi=['chips', 'fuel_can', 'coffe'];
 
-  static const double fallingSpeed = 90;
+  static const double fallingSpeed = 150;
   static const double screenHeight = 320;
+
+
+  double x=0, y=0;
 
 
 
   IgracFigura igrac;
-  double startingY = 0;
   bool zavrsen=false;
 
   PadajucaFigura(double x0, double y0, Size size, ImageMap imageMap,
       IgracFigura igracFigura)
       : igrac = igracFigura,
         super('assets/${pathovi[new Random().nextInt(pathovi.length)]}.png', x0, y0, size, imageMap) {
+          x=x0;
+          y=y0;
     Vx = 0;
     Vy = fallingSpeed;
-    startingY = y0;
   }
 
   @override
   update(double dt) {
     super.update(dt);
+    x+=Vx*dt;
+    y+=Vy*dt;
+    // double tempX=160+y*5/320*(x-160);
+    // position = Offset(x, y);
     if(position.dy<this.size.height/2) this.visible=false;
     else if(!zavrsen) this.visible=true;
     if (this.visible) {
@@ -46,7 +53,7 @@ class PadajucaFigura extends Figura {
     if (position.dy >= screenHeight) {
       position += const Offset(0,
           -GameState.numberOfCollectibles * GameState.baseCollectibleHeighGap);
-      Random random = Random();
+      // Random random = Random();
       this.visible = true;
       zavrsen=false;
     }
